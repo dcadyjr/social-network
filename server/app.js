a799acf69ac63343876c45e26376dfb89d88fb3e
David Cady Jr <davidcadyjr@mac.com>
2017-06-27 09:49:12 -0500

    created profilesController. moved all routes to profilesController.  Get id route working

--- server/app.js
+++ server/app.js
@@ -8,68 +8,15 @@ var bodyParser = require("body-parser");
 
 	require('./db/db');
 
+var profilesController = require('./controllers/profilesController');
+app.use('/profile', profilesController);
+
 app.use(express.static(path.join(__dirname, "views")));
 
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('views', path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
-
-app.get("/profile", function(request, response){
-	
-	response.render('profile', person);
-})
-
-app.get('/profile/:id', function(request, response){
-	
-	var id = request.params.id
-	UserProfile.findById(id, function(err, profile){
-	response.render('profile', profile);
-
-	})
-	
-})
-
-app.post('/profile', function(request, response){
-	console.log(request.body);
-
-	var profile = new UserProfile({
-		name: request.body.name,
-		city: request.body.city,
-		positiveQuote: request.body.positiveQuote,
-		website: request.body.website,
-		pic: request.body.pic
-	})
-
-	profile.save();
-	response.send('success');
-
-})
-
-app.patch('/profile/:id', function(request, response){
-	var id = request.params.id;
-	UserProfile.findById(id, function(err, profile){
-		profile.name = request.body.name;
-		profile.city = request.body.city;
-		profile.positiveQuote = request.body.positiveQuote;
-		profile.website = request.body.website;
-		profile.pic = request.body.pic;
-
-		profile.save();
-		response.json(profile);
-	})
-
-})
-
-app.delete('/profile/:id', function(request, response){
-
-	var id = request.params.id;
-	UserProfile.findById(id, function(err, profile){
-		profile.remove();
-		response.json('success');
-	})
-})
 
 
 server.listen(3000, function(){
--- server/controllers/profilesController.js
+++ server/controllers/profilesController.js
@@ -1,5 +1,62 @@
 var express = require('express');
 var router = express.Router();
-var UserProfile = require('./models/Profiles');
+var UserProfile = require('../models/Profiles');
 var bodyParser = require("body-parser");
 
+router.use(bodyParser.urlencoded({extended: true}));
+
+router.get("/", function(request, response){
+	
+	response.render('profile', person);
+})
+
+router.get('/:id', function(request, response){
+	
+	var id = request.params.id
+	UserProfile.findById(id, function(err, profile){
+	response.render('profile', profile);
+
+	})
+})
+
+router.post('/', function(request, response){
+	console.log(request.body);
+
+	var profile = new UserProfile({
+		name: request.body.name,
+		city: request.body.city,
+		positiveQuote: request.body.positiveQuote,
+		website: request.body.website,
+		pic: request.body.pic
+	})
+
+	profile.save();
+	response.send('success');
+
+})
+
+router.patch('/:id', function(request, response){
+	var id = request.params.id;
+	UserProfile.findById(id, function(err, profile){
+		profile.name = request.body.name;
+		profile.city = request.body.city;
+		profile.positiveQuote = request.body.positiveQuote;
+		profile.website = request.body.website;
+		profile.pic = request.body.pic;
+
+		profile.save();
+		response.json(profile);
+	})
+
+})
+
+router.delete('/:id', function(request, response){
+
+	var id = request.params.id;
+	UserProfile.findById(id, function(err, profile){
+		profile.remove();
+		response.json('success');
+	})
+})
+
+module.exports = router;
